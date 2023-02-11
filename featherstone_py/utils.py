a80949f433b6cdf251ae20abce13b52eb22ac57b
Nikita Ermolenko <nikita.ermolenko@jetbrains.com>
2023-02-11 13:04:50 +0200

    Add documentation for module utils

--- featherstone_py/utils.py
+++ featherstone_py/utils.py
@@ -2,4 +2,9 @@ from typing import Dict, Any
 
 
 def take_last(data: Dict[Any, Any]) -> Any:
+    """
+    Returns the last inserted element in the dictionary.
+    :param data: Dictionary to take the last element from.
+    :return: last element in the dictionary.-
+    """
     return data[next(reversed(data.keys()))]
