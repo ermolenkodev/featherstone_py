5fd986e1b8fd388493d504ec65b9d0db83bac5ba
Nikita Ermolenko <nikita.ermolenko@jetbrains.com>
2023-02-12 12:53:46 +0200

    Add documentation for simulator module

--- featherstone_py/simulator.py
+++ featherstone_py/simulator.py
@@ -1,4 +1,4 @@
-from typing import Union, List
+from typing import Union, List, Tuple
 
 import numpy as np
 
@@ -7,7 +7,25 @@ from featherstone_py.model import MultibodyModel
 
 
 class Simulator:
-    # TODO add validation of properties
+    """
+    It is a toy forward dynamics simulator which uses the given forward dynamics algorithm to simulate the joints trajectory.\f
+    It uses Euler integration scheme and it is quite inefficient
+
+    Attributes
+    ----------
+    model: MultibodyModel
+        Multibody system model in Featherstone's notation.
+    forward_dynamics: InertiaMatrixMethod
+        Forward dynamics algorithm which is used to calculate the joint accelerations.
+    dt: float
+        Simulation time step.
+    integration_resolution: int
+        Number of integration steps per simulation time step. Effectively, it makes time step dt smaller by factor of integration_resolution.
+    curr_time: float
+        Internal elapsed time, should be reset manually if you want to start simulation from the beginning
+    gravity: np.ndarray
+        3d gravity vector.
+    """  # noqa: D301
     def __init__(self, model: MultibodyModel,
                  forward_dynamics: InertiaMatrixMethod = InverseDynamicsUsingRNEA(),
                  dt: float = 0.01,
@@ -20,7 +38,17 @@ class Simulator:
         self.curr_time = 0.
         self.dt = dt
 
-    def advance_to(self, q_: np.ndarray, qd_: np.ndarray, tau_: np.ndarray, time: float):
+    def advance_to(self, q_: np.ndarray, qd_: np.ndarray, tau_: np.ndarray, time: float) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Advance the simulation to the given time. It uses Euler integration scheme.
+        
+        :param q_: Current joint positions
+        :param qd_: Current joint velocities
+        :param tau_: Current applied joint torques
+        :param time: Time to advance to. It should be greater than the internal time of the simulator.
+        :return: Joint positions and velocities at the requested time
+        :raises ValueError: If the requested time is less than the internal time of the simulator.
+        """  # noqa: D301
         q, qd, tau = q_.copy(), qd_.copy(), tau_.copy()
 
         if time < self.curr_time:
@@ -36,4 +64,11 @@ class Simulator:
                 dt = time - self.curr_time
             self.curr_time += dt
 
-        return q, qd, qdd
+        return q, qd
+
+    def reset_internal_time(self) -> None:
+        """
+        Reset the internal time of the simulator to zero.
+        :return:
+        """
+        self.curr_time = 0.
